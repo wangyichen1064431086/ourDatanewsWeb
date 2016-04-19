09b509e290a8d491a07d3e06c4509382865589c3
wangyichen1064431086 <1064431086@qq.com>
2016-04-19 14:42:18 +0800

    优化完善

--- wetryer.com-1-3/datanews/articles/index/articles.js
+++ wetryer.com-1-3/datanews/articles/index/articles.js
@@ -49,63 +49,87 @@ var EventUtil={
 
 
 /**********************************导航栏 纯手写（也可用bootstrap框架的nav）***************************************/
-var navoptions=document.getElementsByClassName("navoption");
-var dropdown=document.getElementsByClassName("dropdown")[0];
-for(var i=0;i<navoptions.length;i++){
-    if (i==2) {
-        navoptions[i].onmouseenter=chColorShowSub;
-        //navoptions[i].onmouseleave=reColorHideSub;
+(function(){
+    var navoptions=document.getElementsByClassName("navoption");
+    var dropdown=document.getElementsByClassName("dropdown")[0];
+    for(var i=0;i<navoptions.length;i++){
+        if (i==2) {
+            navoptions[i].onmouseenter=chColorShowSub;
+            //navoptions[i].onmouseleave=reColorHideSub;
+        }
+        else{
+            navoptions[i].onmouseenter=changeColor;//注意这里不能用onmouseover和onmouseout,因为这样的话其后代元素会单独再触发一遍
+            navoptions[i].onmouseleave=recoverColor;
+        }
     }
-    else{
-        navoptions[i].onmouseenter=changeColor;//注意这里不能用onmouseover和onmouseout,因为这样的话其后代元素会单独再触发一遍
-        navoptions[i].onmouseleave=recoverColor;
+    
+    function chColorShowSub(event) {
+        event=EventUtil.getEvent(event);
+        var target=EventUtil.getTarget(event);
+        target.style.background='#228B22';
+       // dropdown.style.removeProperty('display');//为何不行？？ //dropdown.style.display='none';也不行
+        dropdown.style.setProperty("display","block");
+      
     }
-}
-
-function chColorShowSub(e) {
-    e.target.style.background='#228B22';
-   // dropdown.style.removeProperty('display');//为何不行？？ //dropdown.style.display='none';也不行
-    dropdown.style.setProperty("display","block");
-  
-}
-
-function changeColor(e) {
-    e.target.style.background='#228B22';
-    navoptions[2].style.setProperty("background-color","#9CCD64");
-    dropdown.style.setProperty("display","none");
-}
-
-function recoverColor(e) {
-    e.target.style.removeProperty('background');
-   
-}
-
-if (dropdown.style.display!='none') {
-    dropdown.onmouseleave=reColorHideSub;//从子菜单移走时，该子菜单消失，母标题恢复非触发颜色
-    var dropoptions=document.querySelectorAll(".dropdown>li");
-    for(var j=0;j<dropoptions.length;j++){
-        dropoptions[j].onmouseenter=changeSubColor;
-        dropoptions[j].onmouseleave=recoverSubColor;
+    
+    function changeColor(event){
+        event=EventUtil.getEvent(event);
+        var target=EventUtil.getTarget(event);
+        target.style.background='#228B22';
+        navoptions[2].style.setProperty("background-color","#9CCD64");
+        dropdown.style.setProperty("display","none");
     }
-}
-
-function reColorHideSub(e) {
-
-    navoptions[2].style.removeProperty('background');
-    e.target.style.setProperty("display","none");
-}
-
-function changeSubColor(e) {
-    e.target.style.setProperty("background-color","#228B22");
-}
-
-function recoverSubColor(e) {
-    e.target.style.setProperty("background-color","#9CCD64");
-}
-
-
+    
+    function recoverColor(event) {
+        event=EventUtil.getEvent(event);
+        var target=EventUtil.getTarget(event);
+        target.style.removeProperty('background');
+       
+    }
+    
+    if (dropdown.style.display!='none') {
+        dropdown.onmouseleave=reColorHideSub;//从子菜单移走时，该子菜单消失，母标题恢复非触发颜色
+        var dropoptions=document.querySelectorAll(".dropdown>li");
+        for(var j=0,len=dropoptions.length;j<len;j++){
+            dropoptions[j].onmouseenter=changeSubColor;
+            dropoptions[j].onmouseleave=recoverSubColor;
+        }
+    }
+    
+    function reColorHideSub(event) {
+        event=EventUtil.getEvent(event);
+        var target=EventUtil.getTarget(event);
+        navoptions[2].style.removeProperty('background');
+        target.style.setProperty("display","none");
+    }
+    
+    function changeSubColor(event) {
+        event=EventUtil.getEvent(event);
+        var target=EventUtil.getTarget(event);
+        target.style.setProperty("background-color","#228B22");
+    }
+    
+    function recoverSubColor(event) {
+        event=EventUtil.getEvent(event);
+        var target=EventUtil.getTarget(event);
+        target.style.setProperty("background-color","#9CCD64");
+    }
+})();
 
 /***************************检测localStorage的welcome*****************************/
+EventUtil.addHandler(window,"load",function(){
+    if (localStorage.getItem("welcome")) {//已在其他页面登陆，则每个页面都显示用户昵称
+        document.getElementById("regi").innerHTML=localStorage.getItem("welcome");
+    }
+});
+
+EventUtil.addHandler(window,"unload",function(){
+    if (localStorage.getItem("welcome")) {
+        localStorage.removeItem("welcome");
+    }
+});
+
+/*
 if (localStorage.getItem("welcome")) {//已在其他页面登陆，则每个页面都显示用户昵称
     document.getElementById("regi").innerHTML=localStorage.getItem("welcome");
 }
@@ -115,109 +139,112 @@ window.onunload=function(){
         localStorage.removeItem("welcome");
     }
 }
-
+*/
 
 /****************************************文章类型列表*************************************/
-var articleOptions=document.querySelectorAll(".articleMenu li");
-var articleContent=document.getElementById("articleContent");
-//Dom污染法
-
-for(var i=0,len=articleOptions.length;i<len;i++){
-    articleOptions[i].index=i;
-    articleOptions[i].onclick=function(){
-      this.style.setProperty("background-color","#9CCD64");
-      for(var i=0;i<len;i++){
-            if (i!=this.index) {
-                articleOptions[i].style.setProperty("background-color","lightgray");
+(function(){
+    var articleOptions=document.querySelectorAll(".articleMenu li");
+    
+    //Dom污染法
+    
+    for(var i=0,len=articleOptions.length;i<len;i++){
+        articleOptions[i].index=i;
+        EventUtil.addHandler(articleOptions[i],"click",function(){
+            this.style.setProperty("background-color","#9CCD64");
+            for(var i=0;i<len;i++){
+                if (i!=this.index) {
+                    articleOptions[i].style.setProperty("background-color","lightgray");
+                }
             }
-        }
-        
-      switch (this.index) {
-            case 0:
-                articleContent.src="../newsOverview/index/index.html";
-                break;
-            case 1:
-                articleContent.src="../workAppreciation/index/index.html";
-                break;
-            case 2:
-                articleContent.src="../treasureHunt/index/index.html";
-                break;
-            default:
-                articleContent.src="../newsOverview/index/index.html";
-        }
-       EventUtil.addHandler(articleContent,"load",initIframeHeight);
-      //先加载完iframe内容，再动态调整iframe的高。因为iframe的加载是非阻塞的，故要这么写。
-    };
-}
-            /*****************iframe自适应高度设置*******************/
-
-
-
-function initIframeHeight(){
-    //var height=document.defaultView.getComputedStyle(articleContent,null).height;//height为css中为iframe设置的高度
-    var realHeight=articleContent.contentDocument.body.scrollHeight;
-    console.log("iframe's realHeight:"+realHeight);
-    
-    
-    articleContent.style.height=String(realHeight)+"px";
-
-    console.log(articleContent.style.height);
-     
-}
-
-
+            var articleContent=document.getElementById("articleContent");
+            switch (this.index) {
+                case 0:
+                    articleContent.src="../newsOverview/index/index.html";
+                    break;
+                case 1:
+                    articleContent.src="../workAppreciation/index/index.html";
+                    break;
+                case 2:
+                    articleContent.src="../treasureHunt/index/index.html";
+                    break;
+                default:
+                    articleContent.src="../newsOverview/index/index.html";
+            }
+             /*****************iframe自适应高度设置*******************/
+            EventUtil.addHandler(articleContent,"load",function(){
+                var realHeight=articleContent.contentDocument.body.scrollHeight;
+                console.log("iframe's realHeight:"+realHeight);
+                
+                articleContent.style.height=realHeight+"px";
+            
+                console.log(articleContent.style.height);    
+            });
+          //先加载完iframe内容，再动态调整iframe的高。因为iframe的加载是非阻塞的，故要这么写。
+        });
+    }
+})();
+ 
 /*********************************登录弹窗****************************************/
-var regiBlock=document.getElementById("changeDiv");
-var regiButton=document.getElementById("beforeRegi");
-regiButton.onclick=function(e){
-    regiBlock.style.setProperty("display","block");
-}
+(function(){
+    var regiBlock=document.getElementById("changeDiv");
+    var regiButton=document.getElementById("beforeRegi");
+    
+    EventUtil.addHandler(regiButton,"click",function(){
+        regiBlock.style.setProperty("display","block");
+    });
+    
+})();
 
+/**************************由主页的“阅读更多”设置的localStorage的readGoto值，激活相应按钮，载入相应iframe*******************************/
+(function(){
+    var readGotoValue=localStorage.getItem("readGoto");
+    //var articleContent=document.getElementById("articleContent");
+    
+    var articleOptions=document.querySelectorAll(".articleMenu li");
+    
+    //两种js的switch语句写法：
+    switch (readGotoValue) {
+        case "option0":
+            articleOptions[0].style.setProperty("background-color","#9CCD64");
+            articleOptions[1].style.setProperty("background-color","lightgray");
+            articleOptions[2].style.setProperty("background-color","lightgray");
+            articleContent.src="../newsOverview/index/index.html";
+            break;
+        case "option1":
+            articleOptions[0].style.setProperty("background-color","lightgray");
+            articleOptions[1].style.setProperty("background-color","#9CCD64");
+            articleOptions[2].style.setProperty("background-color","lightgray");
+            articleContent.src="../workAppreciation/index/index.html";
+            break;
+        case "option2":
+            articleOptions[0].style.setProperty("background-color","lightgray");
+            articleOptions[1].style.setProperty("background-color","lightgray");
+            articleOptions[2].style.setProperty("background-color","#9CCD64");
+            articleContent.src="../treasureHunt/index/index.html";
+            break;
+        default:
+            articleOptions[0].style.setProperty("background-color","#9CCD64");
+            articleOptions[1].style.setProperty("background-color","lightgray");
+            articleOptions[2].style.setProperty("background-color","lightgray");
+            articleContent.src="../newsOverview/index/index.html";
+    }
 
-/**************************读取localStorage的readGoto，激活相应按钮，载入相应iframe*******************************/
-var readGotoValue=localStorage.getItem("readGoto");
-//var articleContent=document.getElementById("articleContent");
-
-
-//两种js的switch语句写法：
-switch (readGotoValue) {
-    case "option0":
-        articleOptions[0].style.setProperty("background-color","#9CCD64");
-        articleOptions[1].style.setProperty("background-color","lightgray");
-        articleOptions[2].style.setProperty("background-color","lightgray");
-        articleContent.src="../newsOverview/index/index.html";
-        break;
-    case "option1":
-        articleOptions[0].style.setProperty("background-color","lightgray");
-        articleOptions[1].style.setProperty("background-color","#9CCD64");
-        articleOptions[2].style.setProperty("background-color","lightgray");
-        articleContent.src="../workAppreciation/index/index.html";
-        break;
-    case "option2":
-        articleOptions[0].style.setProperty("background-color","lightgray");
-        articleOptions[1].style.setProperty("background-color","lightgray");
-        articleOptions[2].style.setProperty("background-color","#9CCD64");
-        articleContent.src="../treasureHunt/index/index.html";
-        break;
-    default:
-        articleOptions[0].style.setProperty("background-color","#9CCD64");
-        articleOptions[1].style.setProperty("background-color","lightgray");
-        articleOptions[2].style.setProperty("background-color","lightgray");
-        articleContent.src="../newsOverview/index/index.html";
-}
+})();
 
 /******************点击导航栏的”精品文章“设置localStorage的readGoto为option0，即从此处进入article页面激活的是新闻资讯*/
-var goodArticle=document.getElementById("goodArticle");
-goodArticle.onclick=function(){
-    localStorage.setItem("readGoto","option0");
-    
-    articleOptions[0].style.setProperty("background-color","#9CCD64");
-    articleOptions[1].style.setProperty("background-color","lightgray");
-    articleOptions[2].style.setProperty("background-color","lightgray");
-    articleContent.src="../newsOverview/index/index.html";
+(function(){
+    var goodArticle=document.querySelector(".navoption:nth-child(3)");
+    var articleOptions=document.querySelectorAll(".articleMenu li");
+    EventUtil.addHandler(goodArticle,"click",function(){
+        localStorage.setItem("readGoto","option0");
+        
+        articleOptions[0].style.setProperty("background-color","#9CCD64");
+        articleOptions[1].style.setProperty("background-color","lightgray");
+        articleOptions[2].style.setProperty("background-color","lightgray");
+        articleContent.src="../newsOverview/index/index.html";
+    });
+})();
 
-
-};
 
 
 
--- wetryer.com-1-3/datanews/index/datanews.js
+++ wetryer.com-1-3/datanews/index/datanews.js
@@ -168,91 +168,42 @@ window.onunload=function(){
 
 
 /***********************************旋转木马效果**********************************************/
-var calButtons=document.querySelectorAll("#myCarButtons button");
-var calContents=new Array();
-var myCalContent=document.getElementById("myCarContent");
-var numBu=calButtons.length;
 
-calContents[0]="<a target='_blank' href='../articles/treasureHunt/2/IntroduceofGephi.html'><img src='pic/calContents1.png' alt='calContents0'/></a>";
-calContents[1]="<a target='_blank' href='../articles/treasureHunt/1/Introduce of treasure hunt.html'><img src='pic/calContents2.png' alt='calContents1'/></a>";
-calContents[2]="<img src='pic/calContents3.png' alt='calContents2'/>";
-myCalContent.innerHTML=calContents[0];//初始化旋转木马版块
-calButtons[0].style.setProperty("background-color","#9CCD64");
-
-
-/****按按钮时出现对应图片********/
- /*
-for(var h=0;h<3;h++){
-    calButtons[h].onclick=function(){//使用了闭包。。而闭包只能取得外层函数任何变量的最后一个值，故h不能取到0~2
-        myCalContent.innerHTML=calContents[h];
-    };
-
-*/
-var count=0;//初始化间隔计时器计时值
-var intervalBu;//时间间隔计时器
-var timeoutBu;//等待计时器
-var showCount=document.getElementById("showCount");//在网页上显示计时数字
-
-for(var i=0;i<numBu;i++){ 
-    calButtons[i].onclick=showCalContent;
-}
-
-function showCalContent(e) {
-    window.clearInterval(intervalBu);//按下按钮时，停止自动切换
+(function(){
+    var calButtons=document.querySelectorAll("#myCarButtons button");
+    var carBtn=document.getElementById("myCarButtons");//选取按钮的上级div作为事件委托的不具体元素
+    var myCalContent=document.getElementById("myCarContent");
+    var calContents=new Array();//设置myCalContent的内容数组
+    var numBu=calButtons.length;//3个按钮，numBu为3
+        
+    var count=0;//初始化间隔计时器计时值
+    var showCount=document.getElementById("showCount");//在网页上显示计时数字
+    var flag=0;//标志是否按下按钮，1为按下，0为未按下
+    var start=null;//标记有无已经开启自动播放，若已开启自动播放，则不要再开启第二次
     
-    var pressedBu=e.target;
+    calContents[0]="<a target='_blank' href='../articles/treasureHunt/2/IntroduceofGephi.html'><img src='pic/calContents1.png' alt='calContents0'/></a>";
+    calContents[1]="<a target='_blank' href='../articles/treasureHunt/1/Introduce of treasure hunt.html'><img src='pic/calContents2.png' alt='calContents1'/></a>";
+    calContents[2]="<img src='pic/calContents3.png' alt='calContents2'/>";
+    myCalContent.innerHTML=calContents[0];//初始化旋转木马版块myCalContent的内容
+    calButtons[0].style.setProperty("background-color","#9CCD64");//初始化第一个旋转木马按钮为浅绿
     
-    for (var i=0;i<numBu;i++) {//虽然按下了某一个按钮，但绑定的函数要处理所有的按钮，这样才不会形成闭包
-        calButtons[i].style.setProperty("background-color","white");
-        calButtons[i].style.setProperty("border","thin solid #9CCD64");
-        if (pressedBu==calButtons[i]) {
-            myCalContent.innerHTML=calContents[i];//按下按钮，myCalContent内容变为相应内容
-            count=i;//按下按钮的同时，计时器计时变量count变为按钮相应索引值
-            showCount.innerHTML=count;///在网页上显示计时数字
-
-        }
-    }
-    pressedBu.style.setProperty("background-color","#9CCD64");
-    
-    timeOutBu=window.setTimeout(//按下按钮等待2000ms后无其他操作，则再次开启间隔计时器intervalBu
-        startIntervalBu,
-        3000
-    );
-    
-    function startIntervalBu() {//开启间隔计时器
-        intervalBu=window.setInterval(//循环切换当前按钮及其对应图片的计时器
-            changeNowBu,
-            3000
-        );
-    }
-    
-}
-
-/**********自动播放，当按钮变色时切换到相应图片****************/
-//思路：初始化为每过一定时间，按钮和图片自动切换
-
-
-intervalBu=window.setInterval(//循环切换当前按钮及其对应图片的计时器
-    changeNowBu,
-    3000
-);
-
- function changeNowBu() {
+    /**********自动播放，每隔3000ms按钮变色，同时切换到相应图片****************/
+    function changeNowBu() {
         showCount.innerHTML=count;///在网页上显示计时数字
 
-        if (count%3==0) {
+        if (count==0) {
             calButtons[0].style.setProperty("background-color","#9CCD64");
             calButtons[1].style.setProperty("background-color","white");
             calButtons[2].style.setProperty("background-color","white");
             myCalContent.innerHTML=calContents[0];
         }
-        else if (count%3==1) {
+        else if (count==1) {
             calButtons[0].style.setProperty("background-color","white");
             calButtons[1].style.setProperty("background-color","#9CCD64");
             calButtons[2].style.setProperty("background-color","white");
             myCalContent.innerHTML=calContents[1];
         }
-        else if (count%3==2) {
+        else if (count==2) {
             calButtons[0].style.setProperty("background-color","white");
             calButtons[1].style.setProperty("background-color","white");
             calButtons[2].style.setProperty("background-color","#9CCD64");
@@ -262,9 +213,41 @@ intervalBu=window.setInterval(//循环切换当前按钮及其对应图片的计
         if (count==3) {
             count=0;
         }
-        
+        if (flag==0) {
+            setTimeout(changeNowBu,3000);
+        }
     }
+    start=setTimeout(changeNowBu,3000);//开启按钮间隔变更函数changeNowBu，changeNowBu中检测flag值决定要不要继续进行下去
+                                //此处采用超时调用代替间隔调用
+    /****按按钮时出现对应图片********/
+
+    EventUtil.addHandler(carBtn,"click",function(event){//使用事件委托
+        event=EventUtil.getEvent(event);
+        var target=EventUtil.getTarget(event);
+       // var pressedBu=e.target;
+        if (target.className=="aniBu") {//只有点到按钮上才有效
+            clearTimeout(start);
+            flag=1;
+            for (var i=0;i<numBu;i++) {//先将所有按钮都处理成白色，浅绿边
+                calButtons[i].style.setProperty("background-color","white");
+                calButtons[i].style.setProperty("border","thin solid #9CCD64");
+            }
+            
+            var n=target.id.slice(-1);//通过按下的按钮的id得知其是第几个按钮，再对按下的按钮进行相应操作
+            myCalContent.innerHTML=calContents[n];
+            count=n;
+            showCount.innerHTML=count;
+            target.style.setProperty("background-color","#9CCD64");
+            
+            start=setTimeout(function(){
+                flag=0;
+                changeNowBu();
+            },3000);////按下按钮等待3000ms后无其他操作，则将flag重设为0,并再次恢复执行按钮间隔变更函数changeNowBu    
+        }
+    });
     
+})();
+
 /**********************内容排列区域图标:鼠标放上去，会形成一波光晕******************************/
 (function () {
     
@@ -317,40 +300,52 @@ intervalBu=window.setInterval(//循环切换当前按钮及其对应图片的计
 
    
 /*********************************登录弹窗****************************************/
-var regiBlock=document.getElementById("changeDiv");
-var regiButton=document.getElementById("beforeRegi");
-regiButton.onclick=function(e){
-    regiBlock.style.setProperty("display","block");
-}
+(function(){
+    var regiBlock=document.getElementById("changeDiv");
+    var regiButton=document.getElementById("beforeRegi");
+    
+    EventUtil.addHandler(regiButton,"click",function(){
+        regiBlock.style.setProperty("display","block");
+    });
+    
+})();
 
 
 /***********************点击“阅读更多”跳转到article页面激活相应按钮******************/
-var readmoreBtns=document.getElementsByClassName("sectionReadMore");
+(function(){
+    var readmoreBtns=document.getElementsByClassName("sectionReadMore");
 
-//用闭包来写
-for (var i=0,len=readmoreBtns.length;i<len;i++) {
-    (function (a) {
-        readmoreBtns[a].onclick=function(){
-            localStorage.setItem("readGoto","option"+a);
-        }
-    })(i);   
-}
+    for (var i=0,len=readmoreBtns.length;i<len;i++) {
+        (function(a){
+            EventUtil.addHandler(readmoreBtns[a],"click",function(){
+                localStorage.setItem("readGoto","option"+a);
+            })
+        })(i);
+    }
+})();
+
 /******************点击导航栏的”精品文章“设置localStorage的readGoto为option0，即从此处进入article页面激活的是新闻资讯*/
-var goodArticle=document.getElementById("goodArticle");
-goodArticle.onclick=function(){
-    localStorage.setItem("readGoto","option0");
-}
+(function(){
+    var goodArticle=document.getElementById("goodArticle");
+    
+    EventUtil.addHandler(goodArticle,"click",function(){
+        localStorage.setItem("readGoto","option0");
+    });
+})();
 
 /*******************点击“我要投稿”按钮，检查是否登录，如没有登录，先登录；如已经登录，跳转到“我的工作室”界面***/
-var iWill=document.getElementsByClassName("iWillBu")[0];
+(function(){
+    var regiBlock=document.getElementById("changeDiv");
+    var iWill=document.querySelector(".iWillBu");
+    EventUtil.addHandler(iWill,"click",function(){
+        if (!(localStorage.getItem("welcome"))) {
+            regiBlock.style.setProperty("display","block");
+        }
+        else{
+            localStorage.setItem("myStudioGoto","option0");
+            window.open("../myStudio/index/index.html","_blank");
+        }
+        
+    });
 
-iWill.onclick=function(){
-    if (!(localStorage.getItem("welcome"))) {
-        regiBlock.style.setProperty("display","block");
-    }
-    else{
-        localStorage.setItem("myStudioGoto","option0");
-        window.open("../myStudio/index/index.html","_blank");
-    }
-    
-}
+})();
